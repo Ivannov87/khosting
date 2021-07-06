cc113685db9fd61bb21682e5d5c5b30341e8ea5d
Ivan Renteria <30631018+Ivannov87@users.noreply.github.com>
2021-07-06 15:44:57 -0500

    update contact process
    send php

--- contact_process.php
+++ contact_process.php
@@ -24,16 +24,18 @@ $body .= "<table style='width: 100%;'>";
 $body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
 $body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
 $body .= "</td></tr></thead><tbody><tr>";
-$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+$body .= "<td style='border:none;'><strong>Nombre:</strong> {$name}</td>";
+$body .= "<td style='border:none;'><strong>Correo electronico:</strong> {$from}</td>";
 $body .= "</tr>";
-$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+$body .= "<tr><td style='border:none;'><strong>Asunto:</strong> {$csubject}</td></tr>";
 $body .= "<tr><td></td></tr>";
 $body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
 $body .= "</tbody></table>";
 $body .= "</body></html>";
 
 $send = mail($to, $subject, $body, $headers);
+
+
 
 
 ?>
